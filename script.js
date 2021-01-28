a3d7699b2a89eca7c6cdf0ab721b04f9d92b8cb4
Drew Weston <awfiscus@comcast.net>
2021-01-27 17:36:47 -0700

    finished working portion of quiz by adding function to event listner on submit button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var questions = [
     },
     {
         title: "On a scale of 1-10 how awesome are they?",
-        answer: "10",
+        answer: "8",
         answers: ["8", "6", "2", "5"]
     },
 
@@ -45,9 +45,7 @@ var startPage = document.querySelector("#startPage")
 
 //This function removes the text from the first page
 function removeFirst() {
-    var body = document.getElementById("body")
-    var page1 = document.getElementById("startPage")
-    body.removeChild(page1);
+    $("#startPage").hide();
     createQuestion()
 
 }
@@ -56,7 +54,8 @@ function correctAnswer() {
     currentQuestion++;
     if(currentQuestion >= 2) {
     endOfGame()  
-    } else {
+    }
+    else {
     createQuestion()
     }
 }
@@ -72,39 +71,63 @@ function createQuestion() {
     var button = document.createElement("button")
     button.setAttribute("class", "answer")
     button.textContent = questions[currentQuestion].answers[i]
-    button.addEventListener("click", correctAnswer)
+    button.addEventListener("click", function() {
+        if(this.textContent !== questions[currentQuestion].answer) {
+            (secondsLeft -= 15)
+            console.log(questions[currentQuestion].answer);
+        }
+        $("h1").hide();
+        $(".answer").hide();
+        correctAnswer()
+    })
     body.appendChild(button)
    }
 }   
 
 function endOfGame() {
+    //creates html for end of game page
     var gameOver = document.createElement("h1")
     gameOver.textContent = "Game Over!!"
+
     var body = document.getElementById("body")
     body.appendChild(gameOver)
+
     var finalScore = document.createElement("h4")
     finalScore.setAttribute("id", "finalScore")
     finalScore.textContent = "Your Final score: " + secondsLeft
     body.appendChild(finalScore)
+
     var initialInput = document.createElement("input")
     initialInput.setAttribute("class", "initialsBar")
     body.appendChild(initialInput)
+
     var initials = document.createElement("h6")
     initials.setAttribute("class", "initials")
     initials.textContent = "Enter Your Initials"
     finalScore.appendChild(initials)
     console.log(initialInput);
+
     var submitButton = document.createElement("button")
     submitButton.setAttribute("class", "submit")
     submitButton.textContent = "Submit HighScore"
     body.appendChild(submitButton)
+    //adds event listener to the submit bar to save the value applied to it and saves it to local storage
     submitButton.addEventListener("click", function() {
         var userInitials = initialInput.value;
-        localStorage.setItem("userInitials", userInitials)  
+        localStorage.setItem("userInitials", userInitials)
+        
+        $(gameOver).hide()
+        $(finalScore).hide()
+        $(initialInput).hide()
+        $(initials).hide()
+        $(submitButton).hide()
+        
+        var finalMessage = document.createElement("h1")
+        finalMessage.textContent = "Reload and Try Again. High Scores are saved!"
+        body.appendChild(finalMessage)
     }) 
 }
 
 //Event Listeners
 startButton.addEventListener("click", countDown)
 startButton.addEventListener("click", removeFirst)
-
